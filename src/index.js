8f905b515eb420a054c1cf1903faae64288051e2
ANDRES SAENZ <SAENZA76@YAHOO.COM>
2017-02-13 19:28:45 -0600

    setting up ytsearch ajax call

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
-
 const API_KEY = 'AIzaSyAFEzYcChrhieAexTihGvXHwX6W6iPGqv8';
+
+YTSearch({ key: API_KEY, term: 'cats'}, function(data){
+	console.log(data);
+});
 
 const App = () => {
 	return (
